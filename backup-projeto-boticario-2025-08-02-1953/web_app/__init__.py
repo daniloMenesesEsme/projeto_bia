0e5b2ce6c2c7dcdc0547673877e34385668e355d
Danilo Meneses <danilo.meneses@esme.com.br>
2025-08-10 19:45:53 -0300

    fix: corrige sintaxe do Chat.js linha 24 e remove backups antigos

--- backup-projeto-boticario-2025-08-02-1953/web_app/__init__.py
+++
@@ -1,1 +0,0 @@
-# Este arquivo torna web_app um módulo Python válido
--- restore_safety_snapshot_20250809-105428/web_app/__init__.py
+++
@@ -1,4 +0,0 @@
-# Módulo web_app - Aplicação Flask do Chatbot
-__version__ = "2.0.0"
-__author__ = "Grupo Boticário"
-__description__ = "Chatbot de Conhecimento para Franqueados"
